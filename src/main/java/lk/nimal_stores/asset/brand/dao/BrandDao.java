e22638a5702728a1bd772d6b4c846c0b07c4d9d7
Rasadari Subasingha <malikarasadarisubasingha@gmail.com>
2021-04-30 08:44:25 +0530

    unversioned files were added

---
+++ src/main/java/lk/nimal_stores/asset/brand/dao/BrandDao.java
@@ -0,0 +1,12 @@
+package lk.nimal_stores.asset.brand.dao;
+
+
+
+import lk.nimal_stores.asset.brand.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandDao extends JpaRepository<Brand, Integer> {
+    Brand findByName(String nic);
+}
